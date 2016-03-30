80197165e962cc62a1a8c4e4393df969d9cf532b
Nick Walker <nick.walker@puppetlabs.com>
2016-03-30 10:09:29 -0700

    Move code from site.pp into a profile
    Prior to this commit, the code to setup bitbucket server was
    contained directly in site.pp for specific use in this vagrant
    stack.
    
    After this commit, the code has been moved into a seperate repo
    where it can be used more generally.
    
    This commit removes the code and instead clones down the new repo
    and includes the new profile module.

--- puppet_code/site.pp
+++ puppet_code/site.pp
@@ -39,36 +39,7 @@ File { backup => 'main' }
 
 node 'stash-server' {
   
-  #The bitbucket module uses archive or staging module
-  #which requires unzip but doesn't seem to install it
-  package { 'unzip':
-    ensure => present,
-  }
-  package { 'git' :
-    ensure => present,
-  }
-  class { 'java' :
-    package => 'java-1.8.0-openjdk-devel',
-  } -> 
-  class { 'postgresql::globals':
-    manage_package_repo => true,
-    version             => '9.4',
-  } ->
-  class { 'postgresql::server': } ->
-#  class { 'bitbucket::gc': }
-#  class { 'bitbucket::facts': }
-  postgresql::server::db { 'bitbucket':
-    user     => 'bitbucket',
-    password => postgresql_password('bitbucket', 'password'),
-  } ->
-
-  class { 'bitbucket':
-    javahome    => '/etc/alternatives/java_sdk',
-    #dev.mode grants a 24-hour license for testing
-    java_opts   => '-Datlassian.dev.mode=true',
-    version     => '4.4.1',
-    require     => [ Package['git', 'unzip'] ]
-  }
+  include rampup_profile_bitbucket_server
 
 }
 
